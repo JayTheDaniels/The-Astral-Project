e3794b19558f7856b78cb75e7b898703bebd98d1
JayTheDaniels <73318911+JayTheDaniels@users.noreply.github.com>
2021-03-03 17:32:53 -0500

    Minor changes
    Adding a bit more documentation to the code

--- Src/Code/Object_Base.gd
+++ Src/Code/Object_Base.gd
@@ -2,7 +2,7 @@ extends StaticBody2D
 
 #This script is the parent of all static game objects
 #Other objects will inherit from this one
-#Overwrite/overload the interaction() function for new objects?
+#Overwrite/overload the interaction() function for new objects
 
 func interaction():
 	print("Is interacting with " + self.name)
--- Src/Code/Portal.gd
+++ Src/Code/Portal.gd
@@ -6,12 +6,16 @@ var normal_scene: String = "res://Src/Levels/Level_Base.tscn"
 #add the next scene level to the root node. Hide everything from this one
 	#this keeps the previous level running in the background
 	#for larger games this would kill memory
-	#for this game, preloading every level for easier transitions is probably easiest
+	#for this game, preloading every level for easier transitions is probably best
 
 func interaction():
 	change_level()
 
 func change_level() -> void:
+	#destroys current level, replaces with opposite version
+	#going forward, best to set this to a variable
+	#if possible, rather than destroying previous level, have them loaded simultaneously
+	#this will mean changing the change_scene() function with something else
 	if get_tree().get_root().get_node("/root/Level_Base"):
 		get_tree().change_scene(astral_scene)
 	elif get_tree().get_root().get_node("/root/Astral_Level_Base"):
