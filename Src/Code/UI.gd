2b84ea40fe236de2cc3f03e95824d94536178f7f
kristjanethics <80068289+kristjanethics@users.noreply.github.com>
2021-03-24 10:36:29 -0400

    Combined Auto-load Files
    Merged "Game" with "Global" auto-load file

--- Src/Code/UI.gd
+++ Src/Code/UI.gd
@@ -6,8 +6,8 @@ onready var lives: Label = get_node("CanvasLayer/Lives")
 #lives are currently displayed by text, replace with cat pic
 
 func _ready() -> void:
-	Game.connect("lives_updated", self, "update_interface")
+	Global.connect("lives_updated", self, "update_interface")
 	update_interface()
 	
 func update_interface() -> void:
-	lives.text = "Remaining Lives: %s" % Game.remaining_lives
+	lives.text = "Remaining Lives: %s" % Global.remaining_lives
