61ef7f6d2a314245658e3092d65619116b802f52
kristjanethics <80068289+kristjanethics@users.noreply.github.com>
2021-03-30 15:03:06 -0400

    Added directional animation to Leela

--- Src/Game Objects/Leela.gd
+++ Src/Game Objects/Leela.gd
@@ -2,13 +2,28 @@ extends KinematicBody2D
 
 var follow_player := false
 var speed = 100
+var velocity = Vector2.ZERO
 onready var dialogue = get_parent().get_node("UI/CanvasLayer/PopupDialog")
 onready var player = get_parent().get_node("Player")
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	if follow_player == true:
 		var dir = (player.global_position - global_position).normalized()
 		move_and_collide(dir * speed * delta)
+		var input_vector = Vector2.ZERO
+		input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+		input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+		input_vector = input_vector.normalized()
+		
+		if input_vector != Vector2.ZERO:
+			animationTree.set("parameters/Idle/blend_position", input_vector)
+			animationTree.set("parameters/Walk/blend_position", input_vector)
+			animationState.travel("Walk")
+		else:
+			animationState.travel("Idle")
 
 func interaction():
 	print("Is interacting with " + self.name)
